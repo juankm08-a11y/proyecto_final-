e07713384358f4aac454f79d76e33662d62f3e43
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-03 11:28:22 -0500

    update asistencia-processor

--- backend/asistencia-processor.js
+++ backend/asistencia-processor.js
@@ -12,33 +12,41 @@ const RABBITMQ_URL = "amqp://192.168.80.1";
 
 const EXCHANGE = "aula_exchange";
 const ROUTING_KEY = "asistencia.alerta";
+let rabbitChannel;
+
+async function conectRabbit() {
+  const connection = await amqp.connect(RABBITMQ_URL);
+  const channel = await connection.createChannel();
+  await channel.assertExchange(EXCHANGE, "direct", { durable: false });
+  console.log(`Procesador conectado a rabbitMQ`);
+  return channel;
+}
+
 async function run() {
+  rabbitChannel = await conectRabbit();
   await consumer.connect();
-  await producer.connect();
   await consumer.subscribe({ topic: "asistencia-events", fromBeginning: true });
 
+  console.log("Escuchando eventos Kafka...");
+
   await consumer.run({
-    eachMessage: async ({ message }) => {
-      const data = JSON.parse(message.value);
+    eachMessage: async ({ topic, partition, message }) => {
+      const data = JSON.parse(message.value.toString());
+      console.log(`Evento recibido de Kafka: ${JSON.stringify(data)}`);
+
       const { zoneId, asistanceCount } = data;
 
-      const buffer = zoneBuffers.get(zoneId) || [];
-      buffer.push(asistanceCount);
-      if (buffer.length > 5) buffer.shift();
-      zoneBuffers.set(zoneId, buffer);
+      if (asistanceCount > 40) {
+        const alerta = `Alerta: Zona ${zoneId} tiene alta ocupación (${asistanceCount} personas)`;
+        console.log(alerta);
 
-      const average = buffer.reduce((sum, val) => sum + val, 0) / buffer.length;
-      const summary = {
-        zoneId,
-        currentAverage: average.toFixed(1),
-        timestamp: data.timestamp,
-      };
-
-      await producer.send({
-        topic: "asistencia-summary",
-        messages: [{ value: JSON.stringify(summary) }],
-      });
-      console.log(`Procesando: ${JSON.stringify(summary)}`);
+        rabbitChannel.publish(
+          EXCHANGE,
+          "asistencia.alerta",
+          Buffer.from(alerta)
+        );
+        console.log(`Alerta enviada a RabbitMQ: ${alerta}`);
+      }
     },
   });
 }
