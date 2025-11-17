af3e734e508b98dff7534f26c92dfcceec46de77
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-17 14:26:34 -0500

    Update index.html

--- backend-plataforma_apuestas_deportivas/public/app.js
+++ backend-plataforma_apuestas_deportivas/public/app.js
@@ -1,30 +1,30 @@
-async function loadMatches() {
-  try {
-    const res = await fetch("/matches");
-    if (!res.ok) {
-      throw new Error(`Error al cargar partidos: ${res.status}`);
-    }
+// async function loadMatches() {
+//   try {
+//     const res = await fetch("/matches");
+//     if (!res.ok) {
+//       throw new Error(`Error al cargar partidos: ${res.status}`);
+//     }
 
-    const matches = await res.json();
-    const container = document.getElementById("matches");
-    container.innerHTML = "";
+//     const matches = await res.json();
+//     const container = document.getElementById("matches");
+//     container.innerHTML = "";
 
-    matches.forEach((m) => {
-      const div = document.createElement("div");
+//     matches.forEach((m) => {
+//       const div = document.createElement("div");
 
-      div.className = "match";
-      div.innerHTML = `
-                <strong>${m.homeTeam}</strong> vs <strong>${
-        m.awayTeam
-      }</strong><br>
-                Inicio: ${new Date(m.startAt).toLocaleString()}
+//       div.className = "match";
+//       div.innerHTML = `
+//                 <strong>${m.homeTeam}</strong> vs <strong>${
+//         m.awayTeam
+//       }</strong><br>
+//                 Inicio: ${new Date(m.startAt).toLocaleString()}
 
-            `;
-      container.appendChild(div);
-    });
-  } catch (err) {
-    console.error(err);
-  }
-}
+//             `;
+//       container.appendChild(div);
+//     });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
 
-loadMatches();
+// loadMatches();
