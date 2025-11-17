d4ef9d4931a9f0909c27af3a25fa7355e65dfade
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-17 06:18:49 -0500

    Update dashboard page

--- frontend-plataforma_apuestas_deportivas/src/app/pages/dashboard/dashboard.component.ts
+++ frontend-plataforma_apuestas_deportivas/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Alert } from '../../models/alert.model';
+import { Subscription } from 'rxjs';
+import { AlertController } from '../../controllers/alert.controller';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
+  alerts: Alert[] = [];
+  subscription!: Subscription;
 
+  constructor(private alertCtrl: AlertController) {}
+
+  ngOnInit() {
+    this.subscription = this.alertCtrl.listenAlerts().subscribe((alert) => {
+      this.alerts.unshift(alert);
+      if (this.alerts.length > 10) this.alerts.pop();
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
