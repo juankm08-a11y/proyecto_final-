a2f9c1eb9847b70a168e381145796e4990294b3e
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-16 20:58:00 -0500

    creando componentes: odds-form

---
+++ frontend-plataforma_apuestas_deportivas/src/app/components/odds-form/odds-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-odds-form',
+  imports: [],
+  templateUrl: './odds-form.component.html',
+  styleUrl: './odds-form.component.scss'
+})
+export class OddsFormComponent {
+
+}
