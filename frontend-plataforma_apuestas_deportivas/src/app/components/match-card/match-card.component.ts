40f10d15e55994a997d12d5c7f62b3e68511b21a
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-16 20:57:26 -0500

    creando componentes: match-card

---
+++ frontend-plataforma_apuestas_deportivas/src/app/components/match-card/match-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match-card',
+  imports: [],
+  templateUrl: './match-card.component.html',
+  styleUrl: './match-card.component.scss'
+})
+export class MatchCardComponent {
+
+}
