175c4c7d106b283b23c9b5ada6d75c94c5700730
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-17 05:56:27 -0500

    import common module

--- frontend-plataforma_apuestas_deportivas/src/app/components/match-card/match-card.component.ts
+++ frontend-plataforma_apuestas_deportivas/src/app/components/match-card/match-card.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Match } from '../../models/match.model';
+import {
+  CommonModule,
+  NgIf,
+} from '../../../../node_modules/@angular/common/common_module.d-NEF7UaHr';
 
 @Component({
   selector: 'app-match-card',
-  imports: [],
+  imports: [NgIf, CommonModule],
   templateUrl: './match-card.component.html',
   styleUrl: './match-card.component.scss',
 })
