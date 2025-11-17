9ea061a854c9a733e135d93f49cd58acecc2ccbf
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-11-17 06:06:15 -0500

    update componente odds form

--- frontend-plataforma_apuestas_deportivas/src/app/components/odds-form/odds-form.component.ts
+++ frontend-plataforma_apuestas_deportivas/src/app/components/odds-form/odds-form.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { OddsController } from '../../controllers/odds.controller';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-odds-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './odds-form.component.html',
-  styleUrl: './odds-form.component.scss'
+  styleUrl: './odds-form.component.scss',
 })
 export class OddsFormComponent {
+  @Input() matchId!: string;
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder, private oddsCtrl: OddsController) {
+    this.form = this.fb.group({
+      newOdds: ['', Validators.required],
+    });
+  }
+
+  submit() {
+    if (this.form.valid) {
+      this.oddsCtrl
+        .updateOdds(this.matchId, this.form.value.newOdds)
+        .subscribe({
+          next: (res) => alert('Odds actualizadas!'),
+          error: (err) => alert('Error al actualizar odds'),
+        });
+    }
+  }
 }
